fbaceb6d293799885e758125a270d60fc345070e
Andrey Zraevskii <a.zraevskii@gmail.com>
2021-09-28 19:58:01 +0300

    Мой первый учебный коммит

---
+++ Lesson_1/Class_Work/index.js
@@ -0,0 +1,22 @@
+let namePlanet = "Земля";
+console.log(namePlanet);
+
+let currentTime = "18 сентября 2021 год";
+console.log(currentTime);
+
+let numberArticles = 5;
+console.log(numberArticles);
+
+let isCashPayment = "да";
+console.log(isCashPayment);
+
+let userSurname = "Зраевский";
+console.log(userSurname);
+
+let userName = "Андрей";
+console.log(userName);
+
+let userPatronymis = "Александрович";
+console.log(userPatronymis);
+
+console.log("hello work");
---
+++ Lesson_1/Home_Work/index.js
@@ -0,0 +1,74 @@
+// Вывести в терминал строку “Hello World!”
+// let helloWorld = "Hello World!";
+
+// console.log(helloWorld);
+
+// Создать переменную для каждого типа данных
+// Вывести в консоль редактора кода созданные переменные
+// let age = 34;
+// console.log(age);
+// const userName = "Andrey";
+// console.log(userName);
+// let isGreater = 4 > 1;
+// console.log(isGreater);
+// let planet = null;
+// console.log(planet);
+// let city;
+// console.log(city);
+// let user = {
+//   avto: "Tesla",
+//   model: "s3",
+// };
+// console.log(user);
+// let id = Symbol("id");
+// console.log(id);
+// let bigint = 1n;
+// console.log(bigint);
+
+// Вывести ее тип через оператор typeof и функцию typeof
+// let age = 34;
+// console.log(typeof age);
+// const userName = "Andrey";
+// console.log(typeof userName);
+// let isGreater = 4 > 1;
+// console.log(typeof isGreater);
+// let planet = null;
+// console.log(typeof planet);
+// let city;
+// console.log(typeof city);
+// let user = {
+//     avto: "Tesla",
+//     model: "s3"
+// }
+// console.log(typeof user);
+// let id = Symbol("id");
+// console.log(typeof id);
+// let bigint = 1n
+// console.log(typeof bigint);
+
+// Попробовать изменить переменную объявленную через const
+// const userName = "Max";
+// userName = "Jon";
+// console.log(userName);
+
+// Попробовать изменить переменную с типом object объявленную через const
+// const user = {
+//     name: "Tom"
+// };
+// user.name = "Alex";
+
+// alert(user.name);
+
+// Попробовать изменить переменную с типом object объявленную через let
+// let user = {
+// name: "Jon"};
+// user.name = "Max";
+
+// console.log(user.name);
+
+// Попробовать изменить переменную с типом object объявленную через var
+// var user = {
+// name: "Jon"};
+// user.name = "Alex";
+
+// console.log(user.name);
