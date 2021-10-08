d84aa23c81e6facc75b6385f854c589f200693e4
Andrey Zraevskii <a.zraevskii@gmail.com>
2021-10-08 22:14:07 +0300

    домашняя работа

---
+++ Lesson_3/Class_Work/index.js
@@ -0,0 +1,81 @@
+// String
+
+// const firstValue = String(5);
+
+// console.log(typeof firstValue);
+
+// const obj = {
+//   foo: 67,
+// };
+
+// console.log(typeof String(obj.foo));
+
+// Number
+
+// let testValue;
+
+// console.log(typeof testValue);
+
+// testValue = Number(testValue);
+
+// console.log(testValue);
+
+// const isUser = true;
+// const isAdmin = false;
+
+// console.log(Number(isUser), Number(isAdmin));
+
+// const voidString = "";
+
+// console.log(Number(voidString));
+
+// Boolean
+
+// const value = "12";
+
+// console.log(typeof Boolean(value), Boolean(value));
+
+// const value = "             ";
+
+// console.log(typeof Boolean(value), Boolean(value));
+
+// const obj = [1].length;
+// console.log(Boolean(obj));
+
+// console.log(2 ** 2);
+
+//Унарный плюс
+
+// const value = 5;
+
+// console.log(typeof value, typeof +value);
+
+// const value = "5";
+
+// console.log(typeof value, typeof +value, typeof Number(value));
+
+//Бинарный плюс
+
+// let sum = 5 + 6;
+// console.log(sum);
+
+// sum = 1 + true;
+// console.log(sum);
+
+// sum = 5 + null;
+// console.log(sum);
+
+// const stringValue = "Alex";
+// const numberValue = 34;
+
+// console.log(+stringValue + numberValue);
+// console.log(typeof (numberValue + +stringValue));
+
+// const firstName = "Alex";
+// const lastName = "Shirokov";
+
+// console.log(firstName + " " + lastName);
+
+// console.log(`${firstName} ${lastName}`);
+
+// Операции сравнения
